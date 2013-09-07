db15009183d8323094458b30dfecd791a78def34
Pieter Beulque <pieterbeulque@gmail.com>
2013-09-07 02:30:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Creative Coding: Vooruit 100
+
+The first weekend of september I participated in a Creative Coding bootcamp, three days of coding in whatever language you wanted to make whatever you could imagine, as long as it had some kind of counting element in it. I only worked about 1.5 days because of other work that got in the way, but the experience was fun!
+
+Being my first time doing something like this, I decided to play safe and work with HTML5 Canvas. Not that I'm a huge expert, but at least I've got some weeks of experience with it.
+
+I decided to try out [Sketch.js](http://soulwire.github.io/sketch.js/) library. Sketch is a layer on a browser rendering canvas (be it Three.js or WebGL or Canvas) that should make some things more convenient by providing some extra methods like a drawing loop.
+
+# What I built
+My goal was to make each number morph into the next number in some kind of organic way. I don't think I succeeded 100%, but it's not a big fail either.
+
+I started by defining 20 points on every number character of the Sullivan font. That way I could render every character with simple `moveTo` and `lineTo` calls.
+
+For the transitions I calculated the vector between a point on the current character and the same point on the next character (simplified `current[n].x` to `current[n+1].x`). Using this vector and a step counter (increased on every frame), it's very simple to calculate the current point in the transition. Using the step variable in the transition calculation also allows for organic motion and easing functions.
+
+When I worked that out, the rest was only a matter of keeping a global counter.
+
+For performance I put every character in its own canvas, that way the pixels drawing the final 1 in 100 won't be redrawing every 16 milliseconds.
+
+# Credits
+
+First of all, a big thanks to [@recyclerobot](http://twitter.com/recyclerobot) (and [We Work We Play](http://weworkweplay.com)) for 1) having me learn all kinds of things, including HTML5 canvas, 2) inviting me over, 3) being cool people to work with. Also thanks to [@zuppaman](http://twitter.com/zuppaman) for setting up the whole Creative Coding.
+
+[Sketch.js](http://soulwire.github.io/sketch.js/) by [@soulwire](http://soulwire.github.io).
+
+[Sullivan](http://www.losttype.com/font/?name=sullivan) font found on [Lost Type](http://losttype.com).
+
+Built using [Hammer](http://hammerformac.com).
---
+++ assets/js/app.js
@@ -0,0 +1,392 @@
+window.points = [
+    [
+        {x: 188, y: 75},
+        {x: 155, y: 75},
+        {x: 155, y: 248},
+        {x: 165, y: 248},
+        {x: 165, y: 293},
+        {x: 138, y: 293},
+        {x: 110, y: 265},
+        {x: 110, y: 200},
+        {x: 110, y: 120},
+        {x: 110, y: 58},
+        {x: 138, y: 31},
+        {x: 205, y: 31},
+        {x: 233, y: 58},
+        {x: 233, y: 120},
+        {x: 233, y: 200},
+        {x: 233, y: 265},
+        {x: 205, y: 293},
+        {x: 178, y: 293},
+        {x: 178, y: 248},
+        {x: 188, y: 248}
+    ],
+    [
+        {x: 131, y: 81},
+        {x: 131, y: 81},
+        {x: 131, y: 81},
+        {x: 131, y: 81},
+        {x: 120, y: 38},
+        {x: 120, y: 38},
+        {x: 120, y: 38},
+        {x: 195, y: 27},
+        {x: 195, y: 27},
+        {x: 195, y: 293},
+        {x: 195, y: 293},
+        {x: 195, y: 293},
+        {x: 195, y: 293},
+        {x: 195, y: 293},
+        {x: 195, y: 293},
+        {x: 150, y: 293},
+        {x: 150, y: 293},
+        {x: 150, y: 293},
+        {x: 150, y: 293},
+        {x: 150, y: 79}
+    ],
+    [
+        {x: 193, y: 247},
+        {x: 193, y: 216},
+        {x: 238, y: 226},
+        {x: 238, y: 293},
+        {x: 115, y: 293},
+        {x: 115, y: 166},
+        {x: 142, y: 139},
+        {x: 192, y: 139},
+        {x: 192, y: 75},
+        {x: 160, y: 75},
+        {x: 160, y: 122},
+        {x: 115, y: 111},
+        {x: 115, y: 58},
+        {x: 143, y: 30},
+        {x: 210, y: 30},
+        {x: 238, y: 58},
+        {x: 238, y: 156},
+        {x: 210, y: 184},
+        {x: 160, y: 184},
+        {x: 160, y: 247}
+    ],
+    [
+        {x: 115, y: 111},
+        {x: 115, y: 58},
+        {x: 143, y: 30},
+        {x: 210, y: 30},
+        {x: 238, y: 58},
+        {x: 238, y: 264},
+        {x: 210, y: 293},
+        {x: 142, y: 293},
+        {x: 115, y: 265},
+        {x: 115, y: 211},
+        {x: 160, y: 200},
+        {x: 160, y: 247},
+        {x: 193, y: 247},
+        {x: 193, y: 183},
+        {x: 161, y: 183},
+        {x: 161, y: 138},
+        {x: 193, y: 138},
+        {x: 193, y: 75},
+        {x: 160, y: 75},
+        {x: 160, y: 121}
+    ],
+    [
+        {x: 107, y: 184},
+        {x: 107, y: 120},
+        {x: 107, y: 80},
+        {x: 107, y: 60},
+        {x: 107, y: 41},
+        {x: 152, y: 30},
+        {x: 152, y: 139},
+        {x: 185, y: 139},
+        {x: 185, y: 30},
+        {x: 230, y: 30},
+        {x: 230, y: 65},
+        {x: 230, y: 100},
+        {x: 230, y: 139},
+        {x: 260, y: 139},
+        {x: 260, y: 184},
+        {x: 230, y: 184},
+        {x: 230, y: 293},
+        {x: 185, y: 293},
+        {x: 185, y: 220},
+        {x: 185, y: 184}
+    ],
+    [
+        {x: 230, y: 30},
+        {x: 230, y: 75},
+        {x: 150, y: 75},
+        {x: 150, y: 123},
+        {x: 200, y: 123},
+        {x: 230, y: 150},
+        {x: 230, y: 214},
+        {x: 230, y: 265},
+        {x: 200, y: 293},
+        {x: 135, y: 293},
+        {x: 107, y: 265},
+        {x: 107, y: 211},
+        {x: 152, y: 200},
+        {x: 152, y: 247},
+        {x: 184, y: 247},
+        {x: 184, y: 168},
+        {x: 134, y: 168},
+        {x: 107, y: 141},
+        {x: 107, y: 81},
+        {x: 107, y: 30}
+    ],
+    [
+        {x: 185, y: 75},
+        {x: 151, y: 75},
+        {x: 151, y: 248},
+        {x: 184, y: 248},
+        {x: 184, y: 184},
+        {x: 165, y: 184},
+        {x: 165, y: 138},
+        {x: 201, y: 138},
+        {x: 230, y: 166},
+        {x: 230, y: 265},
+        {x: 202, y: 293},
+        {x: 134, y: 293},
+        {x: 107, y: 265},
+        {x: 107, y: 100},
+        {x: 107, y: 58},
+        {x: 134, y: 30},
+        {x: 202, y: 30},
+        {x: 230, y: 58},
+        {x: 230, y: 111},
+        {x: 185, y: 121}
+    ],
+    [
+        {x: 221, y: 71},
+        {x: 211, y: 131},
+        {x: 205, y: 165},
+        {x: 198, y: 201},
+        {x: 190, y: 249},
+        {x: 182, y: 293},
+        {x: 157, y: 293},
+        {x: 137, y: 293},
+        {x: 145, y: 245},
+        {x: 154, y: 195},
+        {x: 166, y: 129},
+        {x: 175, y: 75},
+        {x: 142, y: 75},
+        {x: 117, y: 75},
+        {x: 113, y: 52},
+        {x: 109, y: 30},
+        {x: 135, y: 30},
+        {x: 162, y: 30},
+        {x: 196, y: 30},
+        {x: 229, y: 30}
+    ],
+    [
+        {x: 232, y: 184},
+        {x: 155, y: 184},
+        {x: 155, y: 248},
+        {x: 188, y: 248},
+        {x: 188, y: 192},
+        {x: 232, y: 192},
+        {x: 232, y: 265},
+        {x: 205, y: 293},
+        {x: 137, y: 293},
+        {x: 109, y: 265},
+        {x: 109, y: 140},
+        {x: 188, y: 140},
+        {x: 188, y: 75},
+        {x: 155, y: 75},
+        {x: 155, y: 129},
+        {x: 109, y: 129},
+        {x: 109, y: 58},
+        {x: 137, y: 30},
+        {x: 205, y: 30},
+        {x: 232, y: 58}
+    ],
+    [
+        {x: 138, y: 183},
+        {x: 110, y: 156},
+        {x: 110, y: 58},
+        {x: 138, y: 30},
+        {x: 206, y: 30},
+        {x: 233, y: 58},
+        {x: 233, y: 120},
+        {x: 233, y: 265},
+        {x: 206, y: 293},
+        {x: 138, y: 293},
+        {x: 110, y: 265},
+        {x: 110, y: 212},
+        {x: 155, y: 205},
+        {x: 155, y: 250},
+        {x: 188, y: 250},
+        {x: 188, y: 75},
+        {x: 155, y: 75},
+        {x: 155, y: 138},
+        {x: 180, y: 138},
+        {x: 180, y: 183}
+    ]
+];
+
+function createCounter () {
+    var s = Sketch.create({
+        autoclear: true,
+        fullscreen: false,
+        autostart: true,
+        autopause: false,
+        container: $('#counter').get(0),
+        setup: function () {
+            this.duration = 37;
+
+            this.step = 0;
+            this.vector = [];
+            this.setCurrentNumber(0);
+        },
+        setCurrentNumber: function (i) {
+            i = (i < 0) ? window.points.length - 1 : i;
+            i = (i >= window.points.length) ? 0 : i;
+
+            this.currentIndex = i;
+
+            this.current = window.points[this.currentIndex];
+
+            var nextIndex = this.currentIndex + 1;
+            if (nextIndex > window.points.length - 1) {
+                nextIndex = 0;
+            }
+         
+            this.next = window.points[nextIndex];
+
+            this.calculateVectors();
+        },
+        drawNext: function () {
+            this.step = 0;
+            // this.setCurrentNumber(this.currentIndex + 1);
+            this.start();
+        },
+        calculateVectors: function () {
+            this.vector = [];
+
+            for (var i = 0; i <= this.current.length - 1; i++) {
+                this.vector.push({
+                    x: this.next[i].x - this.current[i].x,
+                    y: this.next[i].y - this.current[i].y
+                });
+            };
+        },
+        drawTransition: function () {
+            var self = this,
+            t = ~~(this.millis / 1000);
+
+            this.save();
+
+            this.beginPath();
+
+            var startX = this.easing(this.step, this.current[0].x, this.vector[0].x, this.duration),
+                startY = this.easing(this.step, this.current[0].y, this.vector[0].y, this.duration);
+
+            this.moveTo(startX, startY);
+            
+            for (var i = 1; i < this.current.length; i++) {
+                var x = this.easing(this.step, this.current[i].x, this.vector[i].x, this.duration),
+                    y = this.easing(this.step, this.current[i].y, this.vector[i].y, this.duration);
+
+                this.lineTo(x, y);
+            }
+
+            this.lineTo(startX, startY);
+
+            this.strokeStyle = '#fff';
+            this.lineWidth = 4;
+            this.lineCap = 'round';
+            this.lineJoin = 'round';
+
+            this.stroke();
+            this.closePath();
+        },
+        draw: function () {
+            var self = this,
+                t = ~~(this.millis / 1000);
+
+            this.step++;
+
+            this.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+            this.drawTransition();
+
+            if (this.step >= this.duration) {
+                this.stop();
+                self.step = 0;
+                self.setCurrentNumber(self.currentIndex + 1);
+                // self.setCurrentNumber(self.currentIndex + 1);
+                // setTimeout(function () {
+                //     self.start();
+                //     self.drawTransition();
+                // }, this.delay);
+            }
+
+        },
+        easing: function (t, b, c, d) {
+            return ~~(c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b);
+        }
+    });
+
+    return s;
+}
+
+$(window).load(function () {
+    var hundreds = createCounter();
+    hundreds.canvas.width = 265;
+    hundreds.canvas.height = 355;
+
+    var tens = createCounter();
+    tens.canvas.width = 265;
+    tens.canvas.height = 355;
+
+    var ones = createCounter();
+    ones.canvas.width = 265;
+    ones.canvas.height = 355;
+
+    hundreds.start();
+    tens.start();
+    ones.start();
+
+    setTimeout(function () {
+        // Draw one frame for stationary 0 0 0
+        hundreds.stop();
+        tens.stop();
+        ones.stop();
+
+        hundreds.step = tens.step = ones.step = 0;
+    }, 16);
+
+    var current = 0;
+
+    setInterval(function () {
+
+        current++;
+
+        if (current % 100 === 0) {
+            hundreds.drawNext();
+        }
+
+        if (current % 10 === 0) {
+            tens.drawNext();
+        }
+        
+        ones.drawNext();
+    }, 2000);
+
+    var parallaxFlag = true,
+        parallaxOffset = 50,
+        windowWidth = window.innerWidth,
+        windowHeight = window.innerHeight
+
+    $(window).on('mousemove', function (e) {
+        if (parallaxFlag) {
+            var parallaxX = ~~(e.clientX / windowWidth * (parallaxOffset << 1) - parallaxOffset * 3),
+                parallaxY = ~~(e.clientY / windowWidth * (parallaxOffset << 1) - parallaxOffset * 3);
+
+            $('body').css('background-position', parallaxX + 'px ' + parallaxY + 'px');
+
+            parallaxFlag = false;
+
+            setTimeout(function () {
+                parallaxFlag = true;
+            }, 16);
+        }
+    });
+});
